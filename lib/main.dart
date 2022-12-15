570d1fa3ca67176b5ce880942580e6495f922a14
Shirinboyev <gayratshirinboyev6707@gmail.com>
2022-12-15 09:08:58 +0500

    3

--- lib/main.dart
+++ lib/main.dart
@@ -1,208 +1,191 @@
+// // import 'package:flutter/material.dart';
+
+// // void main() {
+// //   runApp(MyWidget());
+// // }
+
+// // class MyWidget extends StatefulWidget {
+// //   MyWidget({super.key});
+
+// //   @override
+// //   State<MyWidget> createState() => _MyWidgetState();
+// // }
+
+// // class _MyWidgetState extends State<MyWidget> {
+// //   TextEditingController controller1 = TextEditingController();
+// //   TextEditingController controller2 = TextEditingController();
+// //   TextEditingController controller3 = TextEditingController();
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       home: Scaffold(
+// //         body: Container(
+// //             child: Center(
+// //           child: Column(
+// //             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+// //             children: [
+// //               Row(
+// //                 children: [
+// //                   Expanded(
+// //                       child: Padding(
+// //                         padding: const EdgeInsets.all(30),
+// //                         child: TextField(
+// //                             decoration: InputDecoration(
+// //                               filled: true,
+// //                               fillColor: Colors.red,
+// //                               border: OutlineInputBorder(),
+// //                             ),
+// //                             controller: controller1),
+// //                       )),
+// //                   Expanded(
+// //                     child: Padding(
+// //                       padding: const EdgeInsets.all(30),
+// //                       child: TextField(
+// //                           controller: controller2,
+// //                           decoration: InputDecoration(filled: true,
+// //                               fillColor: Colors.red,
+// //                             border: OutlineInputBorder(),
+// //                           )),
+// //                     ),
+// //                   )
+// //                 ],
+// //               ),
+// //               TextField(controller: controller3,  decoration: InputDecoration(filled: true,
+// //                               fillColor: Colors.red,
+// //                             border: OutlineInputBorder(),
+// //                           )),
+// //               ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans =
+// //                       int.parse(controller1.text) + int.parse(controller2.text);
+// //                   setState(() {
+// //                     controller3.text = ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('+'),
+// //               ),
+// //               ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans =
+// //                       int.parse(controller1.text) - int.parse(controller2.text);
+// //                   setState(() {
+// //                     controller3.text = ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('-'),
+// //               ),
+// //               ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans =
+// //                       int.parse(controller1.text) * int.parse(controller2.text);
+// //                   setState(() {
+// //                     controller3.text = ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('*'),
+// //               ),
+// //               ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans = int.parse(controller1.text) ~/
+// //                       int.parse(controller2.text);
+// //                   setState(() {
+// //                     controller3.text = ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('/'),
+// //               ),    ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans = 1;
+// //                   setState(() {
+// //                     controller1.text= controller2.text= ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('1'),
+// //               ), ElevatedButton(
+// //                 onPressed: () {
+// //                   int ans = 2;
+// //                   setState(() {
+// //                     controller1.text = controller1.text= ans.toString();
+// //                   });
+// //                 },
+// //                 child: Text('2'),
+// //               ),
+// //             ],
+// //           ),
+// //         )),
+// //       ),
+// //     );
+// //   }
+// // }
+
 // import 'package:flutter/material.dart';
 
-// void main() {
-//   runApp(MyText());
-// }
+// void main() => runApp(MyApp());
 
-// class MyText extends StatefulWidget {
-//   const MyText({Key? key}) : super(key: key);
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-//   @override
-//   State<MyText> createState() => _MyTextState();
-// }
-
-// class _MyTextState extends State<MyText> {
-//   TextEditingController _controller = TextEditingController();
-//   String txt = '';
 //   @override
 //   Widget build(BuildContext context) {
 //     return MaterialApp(
-//       home: Scaffold(
-//         body: Center(
-//           child: Container(
-//             color: Color.fromARGB(255, 238, 234, 234),
-//             child: Column(
-//               mainAxisAlignment: MainAxisAlignment.center,
+//       debugShowCheckedModeBanner: false,
+//       home: Calculator(),
+//     );
+//   }
+// }
+
+// class Calculator extends StatefulWidget {
+//   const Calculator({super.key});
+
+//   @override
+//   State<Calculator> createState() => _CalculatorState();
+// }
+
+// class _CalculatorState extends State<Calculator> {
+//   Widget calcbutton(String btntxt,Color btncolor,Color txtcolor){
+//     return Container(
+      
+      
+      
+//       );
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.amber,
+//       appBar: AppBar(
+//         title: Text('Calculator'),
+//         backgroundColor: Colors.red,
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.symmetric(horizontal: 5),
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.end,
+//           children: [
+          
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.end,
 //               children: [
-//                 Row(
-//                   children: [
-//                     Expanded(
-//                       child: Container(
-//                         child: TextField(
-//                           decoration: InputDecoration(
-//                             filled: true,
-//                             fillColor: Colors.white,
-//                             border: OutlineInputBorder(),
-//                           ),
-//                           controller: _controller,
-//                         ),
-//                       ),
-//                     ),
-//                     Expanded(
-//                       child: Padding(
-//                         padding: const EdgeInsets.all(8.0),
-//                         child: TextField(
-//                           decoration: InputDecoration(
-//                               border: OutlineInputBorder(),
-//                               filled: true,
-//                               fillColor: Colors.white),
-//                           controller: _controller,
-//                         ),
-//                       ),
-//                     ),
-//                   ],
-//                 ),
 //                 Padding(
-//                   padding: const EdgeInsets.only(top: 30, bottom: 30),
-//                   child: Container(
-//                       width: 400,
-//                       height: 50,
-//                       color: Color.fromARGB(255, 213, 210, 210),
-//                       child: Center(child: Text(txt))),
+//                   padding: EdgeInsets.all(10),
+//                   child: Text(
+//                     'O',
+//                     textAlign: TextAlign.left,
+//                     style: TextStyle(color: Colors.white, fontSize: 100),
+//                   ),
 //                 ),
-//                 ElevatedButton(
-//                     onPressed: () {
-//                       setState(() {
-//                         txt = _controller.text;
-//                       });
-//                     },
-//                     child: Text('+')),
-//                 Padding(
-//                   padding: const EdgeInsets.all(30),
-//                   child: ElevatedButton(
-//                       onPressed: () {
-//                         setState(() {
-//                           txt = _controller.text;
-//                         });
-//                       },
-//                       child: Text('-')),
-//                 ),
-//                 ElevatedButton(
-//                     onPressed: () {
-//                       setState(() {
-//                         txt = _controller.text;
-//                       });
-//                     },
-//                     child: Text('*')),
-//                 Padding(
-//                   padding: const EdgeInsets.all(30),
-//                   child: ElevatedButton(
-//                       onPressed: () {
-//                         setState(() {
-//                           txt = _controller.text;
-//                         });
-//                       },
-//                       child: Text('/')),
-//                 )
 //               ],
 //             ),
-//           ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [calcbutton('AC', Colors.black, Colors.red)],
+//             )
+//           ],
 //         ),
 //       ),
 //     );
 //   }
 // }
 
-import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
 
-void main() {
-  runApp(MyWidget());
-}
-
-class MyWidget extends StatefulWidget {
-  MyWidget({super.key});
-
-  @override
-  State<MyWidget> createState() => _MyWidgetState();
-}
-
-class _MyWidgetState extends State<MyWidget> {
-  TextEditingController controller1 = TextEditingController();
-  TextEditingController controller2 = TextEditingController();
-  TextEditingController controller3 = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-            child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                      child: Padding(
-                        padding: const EdgeInsets.all(30),
-                        child: TextField(
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: Colors.red,
-                              border: OutlineInputBorder(),
-                            ),
-                            controller: controller1),
-                      )),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(30),
-                      child: TextField(
-                          controller: controller2,
-                          decoration: InputDecoration(filled: true,
-                              fillColor: Colors.red,
-                            border: OutlineInputBorder(),
-                          )),
-                    ),
-                  )
-                ],
-              ),
-              TextField(controller: controller3,  decoration: InputDecoration(filled: true,
-                              fillColor: Colors.red,
-                            border: OutlineInputBorder(),
-                          )),
-              ElevatedButton(
-                onPressed: () {
-                  int ans =
-                      int.parse(controller1.text) + int.parse(controller2.text);
-                  setState(() {
-                    controller3.text = ans.toString();
-                  });
-                },
-                child: Text('+'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  int ans =
-                      int.parse(controller1.text) - int.parse(controller2.text);
-                  setState(() {
-                    controller3.text = ans.toString();
-                  });
-                },
-                child: Text('-'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  int ans =
-                      int.parse(controller1.text) * int.parse(controller2.text);
-                  setState(() {
-                    controller3.text = ans.toString();
-                  });
-                },
-                child: Text('*'),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  int ans = int.parse(controller1.text) ~/
-                      int.parse(controller2.text);
-                  setState(() {
-                    controller3.text = ans.toString();
-                  });
-                },
-                child: Text('/'),
-              ),
-            ],
-          ),
-        )),
-      ),
-    );
-  }
-}
